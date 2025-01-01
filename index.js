0bb6c251ef0d50f30804b754617ca82904d5dd84
Debian <debian@vps-123a5cf6.vps.ovh.net>
2025-01-01 19:03:27 +0000

    update index.js

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
 const core = require('@actions/core');
 const { execSync } = require('child_process');
+const fs = require('fs');
+const os = require('os');
 const path = require('path');
 
 async function deploy() {
   try {
-    // Lire les entrées dynamiques depuis action.yml
+    // Lire les entrées depuis action.yml
     const sshHost = core.getInput('ssh_host');
     const sshUser = core.getInput('ssh_user');
     const sshPrivateKey = core.getInput('ssh_private_key');
     const sitePath = core.getInput('site_path');
 
-    // Correction du répertoire courant
-    console.log('Répertoire courant avant correction :', process.cwd());
-    const actionDirectory = path.resolve(__dirname);
-    process.chdir(actionDirectory);
-    console.log('Répertoire courant après correction :', process.cwd());
+    // Créer un fichier temporaire pour la clé SSH
+    const sshKeyPath = path.join(os.tmpdir(), 'deploy_key');
+    fs.writeFileSync(sshKeyPath, sshPrivateKey, { mode: 0o600 });
+    console.log(`Clé SSH temporaire écrite dans : ${sshKeyPath}`);
 
     // Création de la commande rsync
     const rsyncCommand = `
-      rsync -avz --delete --exclude='.git*' -e "ssh -i /home/debian/.ssh/deploy_key -o StrictHostKeyChecking=no" ./ ${sshUser}@${sshHost}:${sitePath}/
+      rsync -avz --delete --exclude='.git*' -e "ssh -i ${sshKeyPath} -o StrictHostKeyChecking=no" ./ ${sshUser}@${sshHost}:${sitePath}/
     `;
     console.log('Commande rsync :', rsyncCommand);
 
-    // Exécution de la commande rsync localement
+    // Exécution de la commande rsync
     execSync(rsyncCommand, { stdio: 'inherit' });
 
     console.log('Déploiement réussi.');
+
+    // Optionnel : Supprimer la clé SSH temporaire après utilisation
+    fs.unlinkSync(sshKeyPath);
+    console.log('Clé SSH temporaire supprimée.');
   } catch (error) {
     console.error('Erreur pendant le déploiement :', error.message);
     core.setFailed(error.message);
