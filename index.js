b1f623ec46d071344af5df5c2f28254e960744d2
Debian <debian@vps-123a5cf6.vps.ovh.net>
2025-01-01 21:20:56 +0000

    index

--- index.js
+++ index.js
@@ -35,10 +35,26 @@ async function deploy() {
       throw new Error(`Test SSH échoué : ${error.message}`);
     }
 
+try {
+  execSync(testCommand, { stdio: 'inherit' });
+  console.log('Test SSH réussi.');
+} catch (error) {
+  console.error('Erreur pendant le test SSH :', error);
+  core.setFailed(error.message);
+  return;
+}
+
     // Ajouter ici la commande rsync ou d'autres commandes spécifiques
     const rsyncCommand = `
       rsync -avz --delete --exclude='.git*' -e "ssh -v -i ${sshKeyPath} -o StrictHostKeyChecking=no" ./ ${sshUser}@${sshHost}:${sitePath}/
     `;
+
+console.log('Chemin temporaire pour la clé :', sshKeyPath);
+console.log('Commande SSH de test :', testCommand);
+console.log('Commande rsync :', rsyncCommand);
+
+
+
     console.log('Commande rsync :', rsyncCommand);
     execSync(rsyncCommand, { stdio: 'inherit' });
     console.log('Déploiement réussi.');
