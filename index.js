55802b88d7c8a1a398510f0d2497db53352e00f4
Debian <debian@vps-123a5cf6.vps.ovh.net>
2025-01-02 10:25:23 +0000

    update index + delete testfile

--- index.js
+++ index.js
@@ -7,17 +7,13 @@ async function deploy() {
     const sshUser = core.getInput('ssh_user');
     const sitePath = core.getInput('site_path');
 
-    console.log('Test SSH direct');
-    const debugCommand = `ssh -o StrictHostKeyChecking=no ${sshUser}@${sshHost} echo "Connexion test manuelle réussie"`;
-    execSync(debugCommand, { stdio: 'inherit' });
-
-    console.log('Connexion SSH réussie. Déploiement en cours...');
+    console.log('Déploiement en cours...');
     const rsyncCommand = `
       rsync -avz --delete-excluded --exclude='.git*' -e "ssh -o StrictHostKeyChecking=no" ./ ${sshUser}@${sshHost}:${sitePath}/
     `;
     execSync(rsyncCommand, { stdio: 'inherit' });
 
-    console.log('Déploiement réussi.');
+    console.log('Déploiement terminé avec succès.');
   } catch (error) {
     console.error('Erreur pendant le déploiement :', error.message);
     process.exit(1);
